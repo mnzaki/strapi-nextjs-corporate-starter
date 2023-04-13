9e06e3b8bee3524c6124f351c2e2c3cae4195066
Mina Nagy Zaki <mnzaki@gmail.com>
2023-04-14 01:03:19 +0200

    feat(backend): typescript and graphql

---
+++ backend/config/plugins.ts
@@ -0,0 +1,17 @@
+export default {
+  seo: {
+    enabled: true,
+  },
+  graphql: {
+    config: {
+      endpoint: '/graphql',
+      shadowCRUD: true,
+      playgroundAlways: false,
+      depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: true,
+      },
+    },
+  },
+};
